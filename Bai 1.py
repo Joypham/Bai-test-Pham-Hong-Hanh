c1f3ba7b7e1b6043e6b61c48bc1e370953ac189b
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2019-08-07 20:06:00 +0700

    Update Bai 1.py

--- Bai 1.py
+++ Bai 1.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 
 data_url = 'https://raw.githubusercontent.com/TechMaster/luyen-logic/master/movies.csv'
 df = pd.read_csv(data_url)
+# Tách genre
 genre_list = df["genres"].apply(lambda x: x.split('|'))
 flat_list = [item for sublist in genre_list for item in sublist]
+# Đếm các genre theo tần suất xuất hiện 
 counter_genres = Counter(flat_list)
 print(counter_genres)
